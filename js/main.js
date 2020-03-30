43dd8925378fec381cdd3f2bff92ed58dfa7eabf
Nicolo <nicolorizzi1996@gmail.com>
2020-03-30 17:17:49 +0200

    FIXING

--- js/main.js
+++ js/main.js
@@ -4,39 +4,35 @@
  * 1a)CHIEDERE IL DATO IN INPUT ALL'UTENTE
  * 
  * 1b)CONFRONTA IL DATO CON L'ARRAY GIA' ESISTENTE
+ * 
+ * 1c) VISULIZZA ESITO DEL CONTROLLO
  */
 
 
  
 // OTTENGO IL DATO IN INPUT
- var datoInput = prompt('Fornisci la mail di accesso')
+var datoInput = prompt('Fornisci la mail di accesso');
 
 console.log("Il dato ottenuto in input è: " + datoInput);
 
-//datoInput = datoInput.toLowerCase;
+
  // CREAZIONE DI ARRAY 
 
 var mail = ['michele@boolean.careers','fabio@boolean.careers','roberto@boolean.careers'];
-
-console.table(mail);
 
 // CREO IL CICLO FOR PER ARRIVARE FINO ALL'ULTIMO ELEMENTO DELL'ARRAY E CREO LA CONDIZIONE DI CONTROLLO
 var userFound = false;
 for ( var i = 0; i < mail.length; i++ ) {
     var elemento = mail[i];
-    //console.log(elemento);
     if (datoInput == elemento) {
-        alert(datoInput + " E' presente nella lista")
-        console.log('Si')
+        console.log(datoInput + " Email esistente!");
+        alert(datoInput + " Email esistente!");
         userFound = true;
-
     } 
 }
 
-if (userFound === true) {
-    console.log('Utente trovato');
-    
-} else {
-    console.log('Utente non trovato');
-    
+if (userFound === false) {
+    console.log(datoInput + " Utente non trovato");
+    alert(datoInput + " Utente non trovato");
 }
+
