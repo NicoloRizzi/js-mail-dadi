44c027f32141ea9bca0729cf1718dedfd0240435
Nicolo <nicolorizzi1996@gmail.com>
2020-03-30 18:09:28 +0200

    FIXING

--- js/main-dadi.js
+++ js/main-dadi.js
@@ -2,22 +2,20 @@
  * GENERARE UN NUMERO RANDOM DA 1 A 6 SIA PER GIOCATORE CHE PER COMPUTER
  * STABILIRE IL VINCITORE SULLA BASE DI CHI HA IL NUMERO PIU' GRANDE
  */
-// VARIABILI
- var punteggioUtente = 0;
- var punteggioPc = 0;
- var numCasuale = 0;
+
 
  //CREO UNA VARIABILE CHE GENERA NUMERI CASUALI
-punteggioUtente = Math.floor((Math.random()* 6) + 1);
-alert(punteggioUtente);
-punteggioPc = Math.floor((Math.random() * 6) + 1);
-alert(punteggioPc);
+var punteggioUtente = Math.floor((Math.random()* 6) + 1);
+alert("Il tuo punteggio è: " + punteggioUtente);
+
+var punteggioPc = Math.floor((Math.random() * 6) + 1);
+alert("Il punteggio del Pc è: "+ punteggioPc);
 
 if (punteggioUtente > punteggioPc) {
-    alert("Punteggio dell'Utente vince");
+    alert("Punteggio dell'Utente " + punteggioUtente + " VINCE!");
 
 } else if (punteggioUtente == punteggioPc) {
-    alert('Parità');
+    alert("Parità");
 } else {
-    alert ('Pc vince');
+    alert ("Punteggio del PC " + punteggioPc + " VINCE!");
 }
